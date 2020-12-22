f040a65f72e8e7f015cda5511d44b49a91084d2a
Liverworks <zverochek@yahoo.ca>
2020-12-22 22:47:03 +0300

    add stringtie

--- fqtobed.sh
+++ fqtobed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/minimap2/minimap2 -t 12 -ax splice -uf -k14 -G2k --secondary=no /media/ilia/anna/nanomoss.fasta /home/andrey/guppy4/$1/pass/*.fastq | samtools view -bS | samtools sort  -o $1.bam;
+minimap2 -t 12 -ax splice -uf -k14 -G2k --secondary=no moss_ref.fasta $1/pass/*.fastq | samtools view -bS | samtools sort  -o $1.bam;
 bedtools bamtobed  -bed12 -i  $1.bam > $1.bed;
 bedtools merge -s -n -i $1.bed > merge_$1.bed;
 
